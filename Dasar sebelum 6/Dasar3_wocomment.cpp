71458d9c2f2df40654692b66aad7055c8d340527
annisa :b <112236945+Savoryokill@users.noreply.github.com>
2023-12-27 22:27:30 +0700

    Create folder and first project

---
+++ Dasar sebelum 6/Dasar3_wocomment.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main()
+{
+    std::cout <<"Hello World dan Annisa!" << std::endl;
+    std::cin.get(); //biar bisa dijalanin exe di windows tapi gak langsung ketutup, seperti readln. get() ngambil input enter.
+    return 0;
+}
