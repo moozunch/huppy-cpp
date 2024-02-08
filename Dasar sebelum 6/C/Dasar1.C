89e741f68d4e5890156eafca3a65e97b720c2471
annisa :b <112236945+moozunch@users.noreply.github.com>
2024-02-08 20:22:10 +0700

    Delete Dasar sebelum 6/C directory

--- Dasar sebelum 6/C/Dasar1.C
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-// Library, kalau di C++ Include Iostream for STD Library
-
-// Main is a function name that are integrated with C and C++ to marking the start of every program
-int main() {
-   // printf() displays the string inside quotation
-   printf("Hello, World!"); //Very same like pascal
-   return 0; // Is a must
-}
